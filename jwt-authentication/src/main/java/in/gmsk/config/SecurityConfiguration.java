5e66764eb0e6550cf89d996af8d06d69d037ccea
GouthamGuna <ggowthamsankar@gmail.com>
2023-05-04 18:22:33 +0530

    Update SecurityConfiguration.java

--- jwt-authentication/src/main/java/in/gmsk/config/SecurityConfiguration.java
+++ jwt-authentication/src/main/java/in/gmsk/config/SecurityConfiguration.java
@@ -2,8 +2,10 @@ package in.gmsk.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -44,7 +46,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/welcome", "/api/addNewUser").permitAll()
+                .requestMatchers("/api/welcome", "/api/addNewUser", "/api/authenticate").permitAll()
                 .and()
                 .authorizeHttpRequests()
                 .requestMatchers("/api/**").authenticated()
@@ -58,5 +60,10 @@ public class SecurityConfiguration {
         authenticationProvider.setUserDetailsService(userDetailsService());
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
